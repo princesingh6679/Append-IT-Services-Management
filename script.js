5536082530659bee2fb6683648ed1914f71134f3
Prince Kumar Singh <104294560+princesingh6679@users.noreply.github.com>
2024-07-17 09:54:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// add class navbarDark on navbar scroll
+// const header = document.querySelector('.nav');
+
+// window.onscroll = function() {
+//     var top = window.scrollY;
+//     if(top >=100) {
+//         header.classList.add('navDark');
+//     }
+//     else {
+//         header.classList.remove('navrDark');
+//     }
+// }
